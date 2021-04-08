31e40d33ee2f752b4af5ff1127608b23dd1ee52f
arunbhalli <tomarbhalli.com>
2021-04-08 16:50:42 +0200

    source file added

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+const Footer = () => {
+  return (
+    <footer>
+      <p id="footer">Made With React{React.version}</p>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/Projects.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Container,Grid } from "semantic-ui-react";
+import axios from "axios";
+
+class Projects extends Component {
+  state = {
+    projects: [],
+  };
+
+  componentDidMount() {
+    axios.get(".data/projects.json").then((response) => {
+      this.setState({ projects: response.data });
+    });
+  }
+
+  render() {
+    const { projects } = this.state;
+
+    let projectsList = projects.map((project) => {
+      return (
+        <div id={`project-${project.id}`} key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      );
+    });
+    
+    return (
+      <Container>
+        <h1 id="projects-header">My Projects</h1>
+        {projectsList}
+      </Container>
+    );
+  }
+}
+export default Projects;
