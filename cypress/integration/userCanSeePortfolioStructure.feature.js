c672fc2c32b2d661b7e8c46a7d4f6da2db4f78bf
arunbhalli <tomarbhalli.com>
2021-04-08 17:08:48 +0200

    all file updated

--- cypress/integration/userCanSeePortfolioStructure.feature.js
+++
@@ -1,8 +0,0 @@
-describe("Portfolio interface", () => {
-  it("successfully renders", () => {
-    cy.visit("https://localhost:3000");
-    cy.get("#header").should("contain", "My Portfolio");
-    cy.get("#footer").should("contain", "Made with React");
-    cy.get("#hello").should("contain", "Hello World");
-  });
-});
